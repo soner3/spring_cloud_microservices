6782c109090a4aa18c80c20c515b3b649c678a6d
soner3 <sonerastan@icloud.com>
2024-12-16 18:49:41 +0100

    Created Aggregator Service and added Streaming dependencys

---
+++ microservices/product-aggregator/src/main/java/net/sonerapp/product_aggregator/ProductAggregatorApplication.java
@@ -0,0 +1,13 @@
+package net.sonerapp.product_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAggregatorApplication.class, args);
+	}
+
+}
---
+++ microservices/product-aggregator/src/test/java/net/sonerapp/product_aggregator/ProductAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sonerapp.product_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
