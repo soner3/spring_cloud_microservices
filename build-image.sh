448ed20d4868c32a2b9c35a3a3062f4208f814ef
soner3 <sonerastan@icloud.com>
2024-12-23 12:58:23 +0100

    Initialized Configserve project

--- build-image.sh
+++ build-image.sh
@@ -4,6 +4,8 @@ BASE_DIR=$(pwd)
 
 services=("product" "product-aggregator" "recommendation" "review")
 
+infrastructureServices=("configserver")
+
 for service in "${services[@]}"; do
     (
         echo "Building docker images: $service" || exit;
@@ -12,6 +14,15 @@ for service in "${services[@]}"; do
         echo "Finished building $service";
     ) &
 
+done 
+
+for infrastructureService in "${infrastructureServices[@]}"; do
+    (
+        echo "Building docker images: $infrastructureService" || exit;
+        cd "$BASE_DIR/$infrastructureService";
+        ./mvnw clean package spring-boot:build-image || exit;
+        echo "Finished building $infrastructureService";
+    ) &
 done
 wait
 echo "All services have been built successfully"
