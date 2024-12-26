f49af3aa04cd23dfe42eb0de865e5791e858d05c
soner3 <sonerastan@icloud.com>
2024-12-26 19:59:23 +0100

    Added Eurekaserver to compose network

--- build-image.sh
+++ build-image.sh
@@ -4,14 +4,15 @@ BASE_DIR=$(pwd)
 
 services=("product" "product-aggregator" "recommendation" "review")
 
-version=v6
+version=v7
 
-infrastructureServices=("configserver eurekaserver")
+infrastructureServices=("configserver" "eurekaserver")
 
 for service in "${services[@]}"; do
     (
         echo "Building docker images: $service" || exit;
         cd "$BASE_DIR/microservices/$service";
+        ./mvnw clean package;
         docker build -t "soner9/$service:$version" . || exit;
         echo "Finished building $service";
     ) &
@@ -22,6 +23,7 @@ for infrastructureService in "${infrastructureServices[@]}"; do
     (
         echo "Building docker images: $infrastructureService" || exit;
         cd "$BASE_DIR/$infrastructureService";
+        ./mvnw clean package;
         docker build -t "soner9/$infrastructureService:$version" . || exit;
         echo "Finished building $infrastructureService";
     ) &
